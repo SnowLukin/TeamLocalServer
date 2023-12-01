b96b5b1ec619ec86adc25bd6092c95d6c7051ba0
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-12-01 10:20:38 +0300

    Initial Commit

---
+++ TeamLocalServer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeamLocalServer
+//
+//  Created by Snow Lukin on 01.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeamLocalServer/TeamLocalServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamLocalServerApp.swift
+//  TeamLocalServer
+//
+//  Created by Snow Lukin on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TeamLocalServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
